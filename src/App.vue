9902e2fcb2ff3537ce4c266a9002a4cb0951dd1b
Raúl <ralo.dev@pm.me>
2024-01-16 22:19:35 -0600

    Layout update + HD download
    • Updated image size to be adjusted to 3 different screen sizes
    • Modified the script to improve download image quality from 750px `HD` to 2250 `QHD`
    • Fixed the code to reduce hardcoded variables and added constants to easily modify the output size

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,13 @@
   function downloadImage() {
     // Create a canvas to draw the template and uploaded image
     const canvas = document.createElement('canvas');
-    canvas.width = 750;
-    canvas.height = 750;
+    // Set sizes to 3x to get a higher resolution image (default is 750x750)
+    canvas.width = 750 * 3;
+    canvas.height = 750 * 3;
+    const selfie_size = 260 * 3;
+    const selfie_x = 60 * 3;
+    const selfie_y = 150 * 3;
+    const border_radius = 16 * 3;
 
     const ctx = canvas.getContext('2d');
 
@@ -16,7 +21,7 @@
     const templateImage = new Image();
     templateImage.src = template;
     templateImage.onload = () => {
-      ctx.drawImage(templateImage, 0, 0, 750, 750);
+      ctx.drawImage(templateImage, 0, 0, canvas.width, canvas.height);
 
       // Draw the uploaded image at the specified position with cropping and rounded corners
       if (uploadedImage.value) {
@@ -43,19 +48,37 @@
 
           // Draw the cropped image with rounded corners on the right side
           ctx.beginPath();
-          ctx.moveTo(canvas.width - 60 - 16, canvas.height - 150 - 260);
-          ctx.lineTo(canvas.width - 60 - 260 + 16, canvas.height - 150 - 260);
-          ctx.arcTo(canvas.width - 60 - 260, canvas.height - 150 - 260, canvas.width - 60 - 260, canvas.height - 150 - 260 + 16, 16);
-          ctx.lineTo(canvas.width - 60 - 260, canvas.height - 150 - 260 + 260 - 16);
-          ctx.arcTo(canvas.width - 60 - 260, canvas.height - 150 - 260 + 260, canvas.width - 60 - 260 + 16, canvas.height - 150 - 260 + 260, 16);
-          ctx.lineTo(canvas.width - 60 - 16, canvas.height - 150 - 260 + 260);
-          ctx.arcTo(canvas.width - 60, canvas.height - 150 - 260 + 260, canvas.width - 60, canvas.height - 150 - 260 + 260 - 16, 16);
-          ctx.lineTo(canvas.width - 60, canvas.height - 150 - 260 + 16);
-          ctx.arcTo(canvas.width - 60, canvas.height - 150 - 260, canvas.width - 60 - 16, canvas.height - 150 - 260, 16);
+          ctx.moveTo(canvas.width - selfie_x - border_radius, canvas.height - selfie_y - selfie_size);
+          ctx.lineTo(canvas.width - selfie_x - selfie_size + border_radius, canvas.height - selfie_y - selfie_size);
+          ctx.arcTo(
+            canvas.width - selfie_x - selfie_size,
+            canvas.height - selfie_y - selfie_size,
+            canvas.width - selfie_x - selfie_size,
+            canvas.height - selfie_y - selfie_size + border_radius,
+            border_radius
+          );
+          ctx.lineTo(canvas.width - selfie_x - selfie_size, canvas.height - selfie_y - selfie_size + selfie_size - border_radius);
+          ctx.arcTo(
+            canvas.width - selfie_x - selfie_size,
+            canvas.height - selfie_y - selfie_size + selfie_size,
+            canvas.width - selfie_x - selfie_size + border_radius,
+            canvas.height - selfie_y - selfie_size + selfie_size,
+            border_radius
+          );
+          ctx.lineTo(canvas.width - selfie_x - border_radius, canvas.height - selfie_y - selfie_size + selfie_size);
+          ctx.arcTo(
+            canvas.width - selfie_x,
+            canvas.height - selfie_y - selfie_size + selfie_size,
+            canvas.width - selfie_x,
+            canvas.height - selfie_y - selfie_size + selfie_size - border_radius,
+            border_radius
+          );
+          ctx.lineTo(canvas.width - selfie_x, canvas.height - selfie_y - selfie_size + border_radius);
+          ctx.arcTo(canvas.width - selfie_x, canvas.height - selfie_y - selfie_size, canvas.width - selfie_x - border_radius, canvas.height - selfie_y - selfie_size, border_radius);
           ctx.closePath();
 
           ctx.clip(); // Apply clipping to create rounded corners
-          ctx.drawImage(uploadedImg, offsetX, offsetY, cropWidth, cropHeight, canvas.width - 60 - 260, canvas.height - 150 - 260, 260, 260);
+          ctx.drawImage(uploadedImg, offsetX, offsetY, cropWidth, cropHeight, canvas.width - selfie_x - selfie_size, canvas.height - selfie_y - selfie_size, selfie_size, selfie_size);
 
           // Reset the clipping region
           ctx.restore();
@@ -63,7 +86,7 @@
           // Trigger the download of the merged image
           const downloadLink = document.createElement('a');
           downloadLink.href = canvas.toDataURL('image/png');
-          downloadLink.download = 'merged_image.png';
+          downloadLink.download = 'candidato_ONE.png';
           downloadLink.click();
         };
       }
@@ -88,12 +111,11 @@
 <template>
   <main class="mx-auto flex flex-col justify-evenly">
     <div class="bg-[#ff8c2a] py-3">
-      <h1 class="text-5xl text-white text-center font-bold text-balance">Genera tu tarjeta de candidato ONE!</h1>
+      <h1 class="text-4xl text-white text-center font-bold text-balance">Tarjeta Oracle Next Education</h1>
     </div>
-    <div class="flex flex-col max-w-[600px] justify-center text-center mx-auto">
-      <p class="py-5">
-        Sube tu foto y descarga tu tarjeta de candidato ONE!
-        <br />
+    <div class="flex flex-col max-w-[500px] text-center justify-center mx-auto">
+      <h2 class="pt-5 px-5 text-pretty font-semibold text-lg">Sube tu foto y descarga tu tarjeta de candidato ONE!</h2>
+      <p class="pb-5 pt-1 px-5 text-pretty">
         Puedes publicarla en Linkedin, Facebook e Instagram para mostrar que estás en el proceso de selección del Programa ONE. No olvides utilizar el hashtag
         <span class="text-orange-700">#OracleNextEducation</span>,
         <span class="text-orange-700">#helloONEG6</span>
@@ -113,17 +135,11 @@
       </button>
     </div>
     <div class="flex justify-center mt-5">
-      <div class="relative w-[250px] h-[250px] md:w-[750px] md:h-[750px]">
-        <img :src="template" alt="" class="w-[250px] h-[250px] md:w-[750px] md:h-[750px] object-contain" />
-        <img
-          v-if="uploadedImage"
-          :src="uploadedImage"
-          alt="Uploaded Image"
-          class="absolute md:right-[60px] right-[20px] md:bottom-[150px] bottom-[50px] md:w-[260px] w-[87px] h-[87px] md:h-[260px] object-cover rounded-[5px] md:rounded-2xl overflow-hidden" />
-        <span
-          class="absolute md:right-[60px] right-[20px] md:bottom-[150px] bottom-[50px] md:w-[260px] w-[87px] h-[87px] md:h-[260px] object-cover rounded-[5px] md:rounded-2xl overflow-hidden flex justify-center items-center cursor-pointer"
-          @click="$refs.fileInput.click()">
-          <span v-if="!uploadedImage" class="text-gray-400 md:text-2xl text-sm">Sube tu foto</span>
+      <div class="relative template">
+        <img :src="template" alt="" class="template object-contain" />
+        <img v-if="uploadedImage" :src="uploadedImage" alt="Uploaded Image" class="absolute object-cover overflow-hidden selfie" />
+        <span class="absolute object-cover overflow-hidden flex justify-center items-center cursor-pointer selfie" @click="$refs.fileInput.click()">
+          <span v-if="!uploadedImage" class="text-gray-400 lg:text-2xl md:text-xl text-xs text-center w-full">Sube tu foto</span>
         </span>
         <input type="file" @change="uploadImage" accept="image/*" ref="fileInput" class="hidden" />
       </div>
